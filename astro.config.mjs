1fce6ec3fbb01432690f75a57b768f4376b1005c
ErmiaCodev <ermia1303@gmail.com>
2024-07-12 11:40:07 +0330

    initial fix #1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   integrations: [mdx(), sitemap()],
-  site: 'https://ErmiaCodev.github.io',
+  site: 'https://GeekyChunk.github.io',
   output: 'static',
   outDir: 'docs',
   build: {
