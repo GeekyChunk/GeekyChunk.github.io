3de1f8ea1022b40841d3c25e7eb8aa3f689225cb
ErmiaCodev <ermia1303@gmail.com>
2024-07-12 11:36:55 +0330

    initial #2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap()],
+  site: 'https://ErmiaCodev.github.io',
+  output: 'static',
+  outDir: 'docs',
+  build: {
+    assets: 'astro'
+  }
 });
